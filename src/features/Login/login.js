b7967e73fdad028371c6bcc8d6119e6e61901115
Rohit Kumar <rohit.kumar@mpulsemobile.com>
2019-08-20 16:51:22 +0530

    Added email validation

--- src/features/Login/login.js
+++ src/features/Login/login.js
@@ -7,31 +7,37 @@ import { Form, Button, Container } from 'react-bootstrap';
 import "./login.scss";
 import { updateUser, loginRequest } from './reducer';
 import { cloneDeep } from '../../common/utils';
+import * as constants from '../../common/constants';
 
 type Props = {};
 
-export class Login extends React.PureComponent<Props> {
-  handleSubmit = event => {
-    event.preventDefault();
-    const { user, loginRequest, history } = this.props;
-    delete user.isLoggedIn;
-    loginRequest(user, history);
+type State = {};
+
+export class Login extends React.PureComponent<Props, State> {
+
+  state = {
+    isEmailValid: true
   }
 
   render() {
     const { user } = this.props;
+    const { isEmailValid } = this.state;
 
     return (
       <Container className="login-container">
         <Form className="form-wrapper">
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control 
+            <Form.Control
+              required
               type="email"
               name="email"
               placeholder="Enter email"
               value={user.email}
-              onChange={this.onLoginFieldsInput}/>
+              onChange={this.onLoginFieldsInput}
+              onBlur={this.validateEmail}/>
+                
+             {!isEmailValid && <span className="error">Please provide a valid email id.</span>}
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
@@ -41,10 +47,10 @@ export class Login extends React.PureComponent<Props> {
               name="password"
               placeholder="Password" 
               value={user.password}
-              onChange={this.onLoginFieldsInput}/>
+              onChange={this.onLoginFieldsInput} />
           </Form.Group>
 
-          <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+          <Button variant="primary" type="submit" onClick={this.handleSubmit} disabled={!isEmailValid}>
             LOG IN
           </Button>
         </Form>
@@ -56,12 +62,28 @@ export class Login extends React.PureComponent<Props> {
   ///////////////////////////////////////////////////////////////////////
   //  EVENT HANDLERS
   ///////////////////////////////////////////////////////////////////////
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { user, loginRequest, history } = this.props;
+    delete user.isLoggedIn;
+    loginRequest(user, history);
+  }
   onLoginFieldsInput = (e: SyntheticEvent<any>) => {
     const clonedUser = cloneDeep(this.props.user);
     const { name, value} = e.currentTarget;
     clonedUser[name] = value;
     this.props.updateUser(clonedUser);
   }
+
+  validateEmail = (e: SyntheticEvent<any>) => {
+    e.preventDefault();
+    const value = e.currentTarget.value.trim();
+    const isEmailValid = value === '' || constants.EMAILREGEX.test(value);
+  
+    this.setState({ isEmailValid });
+  }
+
 }
 
 
